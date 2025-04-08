9eaebb9eb8855d9058ee07d16e0381c213fcae40
Chris Womack <womackchrisw@gmail.com>
2025-04-07 20:00:45 -0400

    Update default faucet id

--- src/lib/miden/assets/constants.ts
+++ src/lib/miden/assets/constants.ts
@@ -1,4 +1,4 @@
 export const MIDEN_SLUG = 'miden';
 
 export const FAUCET_ID_STORAGE_KEY = 'miden_faucet_id';
-export const DEFAULT_FAUCET_ID = '0xf42ab06ffd227c2000005b2767bc5e';
+export const DEFAULT_FAUCET_ID = '0x6cac8d0daeb7a72000008427d854ff';
