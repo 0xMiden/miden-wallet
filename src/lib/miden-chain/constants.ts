b377987e32aefb8dd7d2aa28f824eea39a214016
Chris Womack <womackchrisw@gmail.com>
2025-07-25 08:37:35 -0400

    Reset url constants, bump version number

--- src/lib/miden-chain/constants.ts
+++ src/lib/miden-chain/constants.ts
@@ -9,19 +9,19 @@ export enum MIDEN_NETWORK_NAME {
 }
 
 export const MIDEN_NETWORK_ENDPOINTS = new Map<string, string>([
-  [MIDEN_NETWORK_NAME.MAINNET, 'http://localhost:57291'], // Placeholder
-  [MIDEN_NETWORK_NAME.TESTNET, 'http://localhost:57291'],
+  [MIDEN_NETWORK_NAME.MAINNET, 'https://api.miden.io'], // Placeholder
+  [MIDEN_NETWORK_NAME.TESTNET, 'https://rpc.testnet.miden.io'],
   [MIDEN_NETWORK_NAME.LOCALNET, 'http://localhost:57291']
 ]);
 
 export const MIDEN_PROVING_ENDPOINTS = new Map<string, string>([
-  [MIDEN_NETWORK_NAME.TESTNET, 'http://localhost:50051'],
+  [MIDEN_NETWORK_NAME.TESTNET, 'https://tx-prover.testnet.miden.io'],
   [MIDEN_NETWORK_NAME.LOCALNET, 'http://localhost:50051']
 ]);
 
 export const MIDEN_NETWORKS: MidenNetwork[] = [
   {
-    rpcBaseURL: 'localhost:57291',
+    rpcBaseURL: 'https://rpc.testnet.miden.io',
     id: MIDEN_NETWORK_NAME.TESTNET,
     name: 'Testnet',
     autoSync: true
