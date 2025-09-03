eb517187baea0e24a8d199cb2fbe6f10fa5d360f
Evan <evangeorgemarshall@gmail.com>
2025-09-02 17:40:55 -0700

    Fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -43,7 +43,6 @@ const appConfig = {
     fullpage: './src/fullpage.tsx',
     options: './src/options.tsx',
     popup: './src/popup.tsx',
-    background: './src/background.ts',
     contentScript: './src/contentScript.ts',
     addToWindow: './src/addToWindow.ts'
   },
@@ -54,7 +53,7 @@ const appConfig = {
     pathinfo: false,
     path: OUTPUT_PATH,
     assetModuleFilename: `static/media/${fileFormat}`,
-    chunkLoading: 'import-scripts',
+    chunkLoading: 'jsonp',
     chunkFormat: 'array-push'
   },
   resolve: {
@@ -112,7 +111,6 @@ const appConfig = {
       resolvePluginsRelativeTo: __dirname,
       formatter: require.resolve('react-dev-utils/eslintFormatter')
     }),
-
     publicAssetsPlugin(PUBLIC_PATH, OUTPUT_PATH, MANIFEST, TARGET_BROWSER),
 
     new WebpackBar({
@@ -211,6 +209,186 @@ const appConfig = {
   }
 };
 
+const backgroundConfig = {
+  mode: process.env.MODE_ENV,
+  devtool: process.env.MODE_ENV === 'production' ? false : 'source-map',
+  cache: {
+    type: 'filesystem',
+    allowCollectingMemory: true
+  },
+  performance: {
+    hints: false
+  },
+  experiments: {
+    asyncWebAssembly: true,
+    syncWebAssembly: true,
+    topLevelAwait: true
+  },
+  entry: {
+    background: './src/background.ts',
+  },
+  devServer: {
+    hot: true
+  },
+  output: {
+    pathinfo: false,
+    path: OUTPUT_PATH,
+    assetModuleFilename: `static/media/${fileFormat}`,
+    chunkLoading: 'import-scripts',
+    chunkFormat: 'array-push'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.wasm'],
+    alias: {
+      lib: path.resolve(__dirname, 'src', 'lib'),
+      app: path.resolve(__dirname, 'src', 'app'),
+      shared: path.resolve(__dirname, 'src', 'shared'),
+      stories: path.resolve(__dirname, 'src', 'stories'),
+      components: path.resolve(__dirname, 'src', 'components'),
+      screens: path.resolve(__dirname, 'src', 'screens'),
+      utils: path.resolve(__dirname, 'src', 'utils')
+    },
+    fallback: {
+      url: false,
+      os: false,
+      path: false,
+      crypto: false,
+      http: false,
+      https: false,
+      buffer: require.resolve('buffer'),
+      stream: require.resolve('stream-browserify'),
+      assert: require.resolve('assert')
+    }
+  },
+  optimization: {
+    minimizer: [
+      `...`, // webpack@5 syntax to extend existing minimizers
+      new CssMinimizerPlugin()
+    ]
+  },
+  plugins: [
+    new Dotenv(),
+    new webpack.EnvironmentPlugin({
+      VERSION: pkg.version
+    }),
+
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer']
+    }),
+
+    new webpack.ProvidePlugin({
+      process: 'process/browser'
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: 'static/styles/[name].css',
+      chunkFilename: 'static/styles/[name].chunk.css'
+    }),
+    new ForkTsCheckerWebpackPlugin(),
+    ...htmlTemplatesPlugins(PUBLIC_PATH),
+    new ESLintPlugin({
+      extensions: ['js', 'mjs', 'jsx', 'ts', 'tsx'],
+      cache: true,
+      resolvePluginsRelativeTo: __dirname,
+      formatter: require.resolve('react-dev-utils/eslintFormatter')
+    }),
+    publicAssetsPlugin(PUBLIC_PATH, OUTPUT_PATH, MANIFEST, TARGET_BROWSER),
+
+    new WebpackBar({
+      name: 'Miden Wallet Background',
+      color: '#634CFF'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(woff|woff2)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: `static/fonts/${fileFormat}`
+        }
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: `static/media/${fileFormat}`
+        }
+      },
+      {
+        test: /\.module\.css$/i,
+        sideEffects: true,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: {
+                getLocalIdent: getCSSModuleLocalIdent
+              }
+            }
+          },
+          'postcss-loader'
+        ]
+      },
+      {
+        test: /\.css$/i,
+        exclude: /\.module\.css$/i,
+        sideEffects: true,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
+          'postcss-loader'
+        ]
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.tsx?$/,
+        use: [
+          {
+            loader: '@svgr/webpack',
+            options: {
+              prettier: false,
+              svgo: false,
+              svgoConfig: {
+                plugins: [{ removeViewBox: false }]
+              },
+              titleProp: true,
+              ref: true
+            }
+          },
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'static/media/[name].[hash].[ext]'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.m?js$/i,
+        exclude: /node_modules/,
+        type: 'javascript/auto'
+      },
+      {
+        test: /\.tsx?$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            customize: require.resolve('babel-preset-react-app/webpack-overrides')
+          }
+        }
+      }
+    ]
+  }
+};
 const workerConfig = {
   mode: process.env.NODE_ENV,
   devtool: process.env.MODE_ENV === 'development' ? 'inline-source-map' : false,
@@ -298,4 +476,4 @@ const workerConfig = {
   }
 };
 
-module.exports = [appConfig, workerConfig];
+module.exports = [appConfig, backgroundConfig, workerConfig];
