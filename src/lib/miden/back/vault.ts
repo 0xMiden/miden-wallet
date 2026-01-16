392314ed3db9eec17befbea488c60c6db59f0e7a
0xnullifier <utkarsh382004@gmail.com>
2026-01-16 13:49:11 +0530

    chore: check for network accounts too

--- src/lib/miden/back/vault.ts
+++ src/lib/miden/back/vault.ts
@@ -319,7 +319,7 @@ export class Vault {
         // Have to do this sequentially else the wasm fails
         for (const accountHeader of accountHeaders) {
           const account = await midenClient.getAccount(getBech32AddressFromAccountId(accountHeader.id()));
-          if (!account || account.isFaucet()) {
+          if (!account || account.isFaucet() || account.isNetwork()) {
             continue;
           }
           accts.push(account);
